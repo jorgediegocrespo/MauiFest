33d59f96befc3ca385c387e986695923c772c913
Jorge Diego <jorge@devsdna.com>
2022-06-08 09:57:42 +0200

    Demo 1

--- MauiFest/MauiFest/Features/MainViewModel.cs
+++ MauiFest/MauiFest/Features/MainViewModel.cs
@@ -3,22 +3,22 @@ using System.Windows.Input;
 using TaskManager.Base;
 
 //TODO 1.3 Preprocessor directives
-//#if ANDROID
-//using Android.Content;
-//#elif IOS
-//using Foundation;
-//using UIKit;
-//#endif
+#if ANDROID
+using Android.Content;
+#elif IOS
+using Foundation;
+using UIKit;
+#endif
 
 namespace MauiFest.Features;
 
 public class MainViewModel : BaseViewModel
 {
     //TODO 1.1 Preprocessor directives
-    //public MainViewModel()
-    //{
-    //    OpenAppSettingsCommand = new Command(OpenAppSetting);
-    //}
+    public MainViewModel()
+    {
+        OpenAppSettingsCommand = new Command(OpenAppSetting);
+    }
 
     //TODO 2.4 Platform services
     //private readonly ISettingsService settingsService;
@@ -31,19 +31,19 @@ public class MainViewModel : BaseViewModel
     public ICommand OpenAppSettingsCommand { get; private set; }
 
     //TODO 1.2 Preprocessor directives
-    //    private void OpenAppSetting(object obj)
-    //    {
-    //#if ANDROID
-    //        var context = global::Android.App.Application.Context;
+    private void OpenAppSetting(object obj)
+    {
+#if ANDROID
+        var context = global::Android.App.Application.Context;
 
-    //        var intent = new Intent(global::Android.Provider.Settings.ActionApplicationDetailsSettings);
-    //        intent.AddFlags(ActivityFlags.NewTask | ActivityFlags.NoHistory | ActivityFlags.ExcludeFromRecents);
-    //        var uri = global::Android.Net.Uri.Parse("package:com.companyname.mauifest");
-    //        intent.SetData(uri);
-    //        context.StartActivity(intent);
-    //#elif IOS
-    //        var url = new NSUrl("app-settings:com.companyname.mauifest");
-    //        UIApplication.SharedApplication.OpenUrl(url);
-    //#endif
-    //    }
+        var intent = new Intent(global::Android.Provider.Settings.ActionApplicationDetailsSettings);
+        intent.AddFlags(ActivityFlags.NewTask | ActivityFlags.NoHistory | ActivityFlags.ExcludeFromRecents);
+        var uri = global::Android.Net.Uri.Parse("package:com.companyname.mauifest");
+        intent.SetData(uri);
+        context.StartActivity(intent);
+#elif IOS
+            var url = new NSUrl("app-settings:com.companyname.mauifest");
+            UIApplication.SharedApplication.OpenUrl(url);
+#endif
+    }
 }
--- MauiFest/MauiFest/Mappers/ButtonMappers.iOS.cs
+++ MauiFest/MauiFest/Mappers/ButtonMappers.iOS.cs
@@ -1,42 +1,60 @@
 ﻿using Microsoft.Maui.Platform;
+using System.Diagnostics;
 
 namespace MauiFest.Mappers;
 
 public class ButtonMappers
 {
     //TODO 4.2 Handlers - mappers
-    public static void CreateHandlers()
+    public static void CreateMappers()
     {
-        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("Border", (handler, view) =>
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("MauiFestMap", (handler, view) =>
         {
-            handler.PlatformView.Layer.CornerRadius = Convert.ToSingle(10);
-            handler.PlatformView.Layer.BorderColor = Colors.Red.ToCGColor();
-            handler.PlatformView.Layer.BorderWidth = 2;
-            handler.PlatformView.ClipsToBounds = true;
+            Debug.WriteLine("***** ModifyMapping MauiFestMap 1");
         });
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("MauiFestMap", (handler, view) =>
+        {
+            Debug.WriteLine("***** ModifyMapping MauiFestMap 2");
+        });
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.PrependToMapping("MauiFestMap", (handler, view) =>
+        {
+            Debug.WriteLine("***** PrependToMapping MauiFestMap");
+        });
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.ModifyMapping("MauiFestMap", (handler, view, action) =>
+        {
+            Debug.WriteLine("***** ModifyMapping MauiFestMap");
+        });
+
+
+
+
 
         Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry.TextColor), (handler, view) =>
         {
+            Debug.WriteLine($"***** AppendToMapping {nameof(Entry.TextColor)}");
             handler.PlatformView.Layer.CornerRadius = Convert.ToSingle(10);
-            handler.PlatformView.Layer.BorderColor = handler.PlatformView.TextColor.CGColor; //view.TextColor.ToCGColor();
+            handler.PlatformView.Layer.BorderColor = view.TextColor.ToCGColor();
             handler.PlatformView.Layer.BorderWidth = 2;
             handler.PlatformView.ClipsToBounds = true;
         });
 
         Microsoft.Maui.Handlers.EntryHandler.Mapper.PrependToMapping(nameof(Entry.TextColor), (handler, view) =>
         {
-            handler.PlatformView.Layer.CornerRadius = Convert.ToSingle(10);
-            handler.PlatformView.Layer.BorderColor = handler.PlatformView.TextColor.CGColor; //view.TextColor.ToCGColor();
-            handler.PlatformView.Layer.BorderWidth = 2;
-            handler.PlatformView.ClipsToBounds = true;
+            Debug.WriteLine($"***** PrependToMapping {nameof(Entry.TextColor)} 1");
+        });
+
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.PrependToMapping(nameof(Entry.TextColor), (handler, view) =>
+        {
+            Debug.WriteLine($"***** PrependToMapping {nameof(Entry.TextColor)} 2");
         });
 
         Microsoft.Maui.Handlers.EntryHandler.Mapper.ModifyMapping(nameof(Entry.TextColor), (handler, view, action) =>
         {
-            handler.PlatformView.Layer.CornerRadius = Convert.ToSingle(10);
-            handler.PlatformView.Layer.BorderColor = view.TextColor.ToCGColor();
-            handler.PlatformView.Layer.BorderWidth = 2;
-            handler.PlatformView.ClipsToBounds = true;
+            Debug.WriteLine($"***** ModifyMapping {nameof(Entry.TextColor)}");
+
             //action?.Invoke(handler, view);
         });
     }
